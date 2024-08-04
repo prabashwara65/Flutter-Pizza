825f85abc6d54abcbf25b23c5843f30019b61e8a
prabashwara65 <prabashwara65@gmail.com>
2024-08-04 12:54:20 +0530

    added bloc files

--- lib/blocs/authentication_bloc/authentication_event.dart
+++ lib/blocs/authentication_bloc/authentication_event.dart
@@ -6,3 +6,12 @@ sealed class AuthenticationEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class AuthenticationUserChanged extends AuthenticationEvent {
+  final MyUser? user;
+
+  const AuthenticationUserChanged(this.user);
+
+  // @override
+  // List<Object> get props => [user];
+}
