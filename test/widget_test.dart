70addb3bdbb44a3157461b78509b9a302ca64380
prabashwara65 <prabashwara65@gmail.com>
2024-08-04 13:24:57 +0530

    confure ios and android for flutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_pizza_app/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_pizza_app/app.dart';
+import 'package:flutter_pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() {
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    Bloc.observer = SimpleBlocObserver();
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(FirebaseUserRepo()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
