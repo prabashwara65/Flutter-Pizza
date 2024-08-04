6d9a79b30d446bbf1566e6213723582b7fee58cb
prabashwara65 <prabashwara65@gmail.com>
2024-08-04 14:53:45 +0530

    fixing gradle prob

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:flutter_pizza_app/screens/auth/views/welcome_screen.dart';
+import 'package:flutter_pizza_app/screens/home/views/home_screen.dart';
 // import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
 // import 'package:pizza_app/screens/auth/blocs/sing_in_bloc/sign_in_bloc.dart';
 // import 'package:pizza_app/screens/home/blocs/get_pizza_bloc/get_pizza_bloc.dart';
 // import 'package:pizza_repository/pizza_repository.dart';
 
-import 'screens/auth/views/welcome_screen.dart';
-import 'screens/home/views/home_screen.dart';
+// import 'screens/auth/views/welcome_screen.dart';
+// import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -21,19 +23,19 @@ class MyAppView extends StatelessWidget {
         home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
           builder: ((context, state) {
             if (state.status == AuthenticationStatus.authenticated) {
-              return MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: (context) => SignInBloc(context.read<AuthenticationBloc>().userRepository),
-                  ),
-                  BlocProvider(
-                    create: (context) => GetPizzaBloc(
-                      FirebasePizzaRepo()
-                    )..add(GetPizza()),
-                  ),
-                ],
-                child: const HomeScreen(),
-              );
+             // return MultiBlocProvider(
+                // providers: [
+                //   BlocProvider(
+                //     create: (context) => SignInBloc(context.read<AuthenticationBloc>().userRepository),
+                //   ),
+                //   BlocProvider(
+                //     create: (context) => GetPizzaBloc(
+                //       FirebasePizzaRepo()
+                //     )..add(GetPizza()),
+                //   ),
+                // ],
+                return const HomeScreen();
+              
             } else {
               return const WelcomeScreen();
             }
