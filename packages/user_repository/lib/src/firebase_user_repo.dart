ac22d326506b886a93caa9e352bfa830b2c4268f
prabashwara65 <prabashwara65@gmail.com>
2024-08-04 10:59:18 +0530

    added firebase

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,7 +1,8 @@
 import 'dart:developer';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+ import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:rxdart/rxdart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:user_repository/src/user_repo.dart';
 import 'package:user_repository/user_repository.dart';
 
