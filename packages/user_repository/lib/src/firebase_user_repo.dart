02ebb8c9f0ec61615ab05e5985830a7eff85930d
prabashwara65 <prabashwara65@gmail.com>
2024-08-04 12:11:18 +0530

    commit

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
  import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:rxdart/rxdart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:user_repository/src/models/models.dart';
 import 'package:user_repository/src/user_repo.dart';
 import 'package:user_repository/user_repository.dart';
 
